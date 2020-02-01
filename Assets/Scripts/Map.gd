292ebd0a83914266994bc5e1ba7bfdcb850cb1d5
Andrew Oatley-Willis <oatleywillisa@gmail.com>
2020-02-01 18:03:24 -0500

    Added lava tiles

--- Assets/Scripts/Map.gd
+++ Assets/Scripts/Map.gd
@@ -1,16 +1,20 @@
 extends Node2D
 
+# Objects for store stupid amounts of data
 var world = {} # map_name:map_file_path
 var maps = {} # map_name: {tile_key:tile} 
 
+# Preloads and prefabs
 onready var gui = get_node("/root/Main/GUI")
 var floor_tile = load("res://Assets/Scenes/Floor.tscn")
 var wall_tile = load("res://Assets/Scenes/Wall.tscn")
 var water_tile = load("res://Assets/Scenes/Water.tscn")
+var lava_tile = load("res://Assets/Scenes/Lava.tscn")
 var sand_tile = load("res://Assets/Scenes/Sand.tscn")
 var tree_tile = load("res://Assets/Scenes/Tree.tscn")
 var player_tile = load("res://Assets/Scenes/Player.tscn")
 
+# Global vars
 var world_name = "land1"
 var world_size_x
 var world_size_y
@@ -18,10 +22,11 @@ var map_size = 50
 var tile_size = 32 # Sprite size
 var player
 
-# Called when the node enters the scene tree for the first time.
+# If you ever need to meow, look no further 
 func _ready():
   """meow"""
 
+# Quick shortcut to run all loads
 func play():
   gui.hide()
   load_world()
@@ -33,6 +38,7 @@ func play():
       draw_map_tiles(map_name) # can you only draw map tiles of the closest 5?
   add_player("0x0x0") 
 
+# Load world file, which contains map names and file paths unique each world
 func load_world():
   var f = File.new()
   f.open("res://Assets/Worlds/"+world_name+"/"+world_name+".world", f.READ)
@@ -45,6 +51,7 @@ func load_world():
   else:
     print("error with json in world")
 
+# Each world is made of dozens of maps, this loads individual maps
 func load_map(map_name):
   var f = File.new()
   f.open(world[map_name], f.READ)
@@ -55,6 +62,7 @@ func load_map(map_name):
   else:
     print("error with json in map", map_name)
 
+# Spawn player object
 func add_player(map_name):
   player = player_tile.instance()
   player.key = "player"
@@ -62,6 +70,7 @@ func add_player(map_name):
   player.update_pos(maps[map_name]["mapsize"]["x"] / 2, maps[map_name]["mapsize"]["y"] / 2)
   add_child(player)
 
+# Contains all compatitble tiles within
 func draw_map_tiles(map_name):
   var node = Node2D.new()
   var map_group_x = int(map_name.split("x")[0])
@@ -72,29 +81,33 @@ func draw_map_tiles(map_name):
   for key in maps[map_name].keys():
     var x = maps[map_name][key]['x']
     var y = maps[map_name][key]['y']
-    if maps[map_name][key]['c'] == "#":
+    if maps[map_name][key]['c'] == "#": # wall_tile
       var t = wall_tile.instance()
       t.position = update_pos(x, y, map_group_x, map_group_y)
       node.add_child(t)
-    elif maps[map_name][key]['c'] == ".":
+    elif maps[map_name][key]['c'] == ".": # stone_floor_tile 
       var t = floor_tile.instance()
       t.position = update_pos(x, y, map_group_x, map_group_y)
       node.add_child(t)
-    elif maps[map_name][key]['c'] == "~":
+    elif maps[map_name][key]['c'] == "~": # water_tile
       var t = water_tile.instance()
       t.position = update_pos(x, y, map_group_x, map_group_y)
       node.add_child(t)
-    elif maps[map_name][key]['c'] == ",":
+    elif maps[map_name][key]['c'] == "=": # lava_tile
+      var t = lava_tile.instance()
+      t.position = update_pos(x, y, map_group_x, map_group_y)
+      node.add_child(t)
+    elif maps[map_name][key]['c'] == ",": # sand_tile
       var t = sand_tile.instance()
       t.position = update_pos(x, y, map_group_x, map_group_y)
       node.add_child(t)
-    elif maps[map_name][key]['c'] == "t":
+    elif maps[map_name][key]['c'] == "t": # tree_tile
       # Add a ground tile under the tree
-      if maps[map_name]["default_floor"]['c'] == ',':
+      if maps[map_name]["default_floor"]['c'] == ',': # floors under trees
         var t = sand_tile.instance()
         t.position = update_pos(x, y, map_group_x, map_group_y)
         node.add_child(t)
-      elif maps[map_name]["default_floor"]['c'] == '.':
+      elif maps[map_name]["default_floor"]['c'] == '.': # floors under trees
         var t = floor_tile.instance()
         t.position = update_pos(x, y, map_group_x, map_group_y)
         node.add_child(t)
@@ -102,6 +115,8 @@ func draw_map_tiles(map_name):
       t.position = update_pos(x, y, map_group_x, map_group_y)
       node.add_child(t)
 
+# Really hacky, relative positional calculation of tiles
+# Formula: x = mapx * tile_size + (tile_size * map_size * map_group_x)
 func update_pos(posx, posy, map_group_x, map_group_y):
   var mapx = posx
   var mapy = posy
